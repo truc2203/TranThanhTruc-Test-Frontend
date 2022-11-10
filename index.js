97d605935c2af4b7db47d43250759c5ba4e70c4a
truc2203 <tranthanhtruc22031998@gmail.com>
2022-11-10 21:07:16 +0700

    responsive

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-// Question 10
-
-const a =  [1,3,4,2,6,7,9]
-const b = [2,3,1,5,7,9]
-let count = []
-
-for(let i=0 ; i <=a.length-1 ;i++)
-{
-    let temp = a[i]
-    for(let j = 0; j<= b.length-1;j++)
-    {
-        if(b[j] === a[i])
-        {
-            count.push(a[i])
-        }
-    }
+const isOpen = () => {
+    
+    let openMenu = document.getElementById('showMenu')
+    let icon = document.getElementById('icon-close')
+    if (openMenu.style.visibility === "hidden") {
+        openMenu.style.visibility = "visible";
+        openMenu.style.height = "180px"
+        openMenu.style.transition = "all 0.3s"
+        icon.classList.remove("fa-solid", "fa-bars")
+        icon.classList.add("fa-solid", "fa-xmark")
+    
+      }
+      else {
+        openMenu.style.visibility = "hidden"
+        openMenu.style.height = "0px"
+        icon.classList.remove("fa-solid", "fa-xmark")
+        icon.classList.add("fa-solid", "fa-bars")
+        openMenu.style.transition = "all 0.3s"
+    
+      }
 }
-
-console.log(count);
---
+++ question10.js
@@ -0,0 +1,19 @@
+// Question 10
+
+const a =  [1,3,4,2,6,7,9]
+const b = [2,3,1,5,7,9]
+let count = []
+
+for(let i=0 ; i <=a.length-1 ;i++)
+{
+    let temp = a[i]
+    for(let j = 0; j<= b.length-1;j++)
+    {
+        if(b[j] === a[i])
+        {
+            count.push(a[i])
+        }
+    }
+}
+
+console.log(count);
